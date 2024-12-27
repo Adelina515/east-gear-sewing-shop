2e59ceaba6fa68eed3a8bd141fd01b598c8321ca
Adelina515 <adelgodlevska@gmail.com>
2024-12-27 15:29:29 +0200

    add tablet files

---
+++ tablet/technologyList.js
@@ -0,0 +1,195 @@
+export const technologyList = [
+  {
+    text: "1. Розмітка центру краю Пришивання блискавки до деталей переньої стінки, шов 7 мм від краю.",
+    img: [
+      "../img/tablet/img1a.jpg",
+      "../img/tablet/img1b.jpg",
+      "../img/tablet/img1c.jpg",
+    ],
+    description: "img1",
+  },
+  {
+    text: "2. Нашивання липучки (10мм від пришитого краю) та бірки на передню стінку, вставка бігунка",
+    img: ["../img/tablet/img2.jpg"],
+    description: "img2",
+  },
+  {
+    text: "3. Пришивання проміжної підкладки на передню стінку. Обрізка залишків підкладки",
+    img: ["../img/tablet/img3.jpg"],
+    description: "img3",
+  },
+  {
+    text: "4. Зшивання захисного бортика під планшет 5ммшов, вставка піни",
+    img: ["../img/tablet/img4a.jpg", "../img/tablet/img4b.jpg"],
+    description: "img4",
+  },
+  {
+    text: "5. Загинання (на 1 см коротшого краю) строчкою деталей петель",
+    img: ["../img/tablet/img5.jpg"],
+    description: "img5",
+  },
+  {
+    text: "6. Розмітка та закріплення пластику до внутрішньої передньої стінки (велкро)",
+    img: ["../img/tablet/img6.jpg"],
+    description: "img6",
+  },
+  {
+    text: "7. Нашивання ПЕТЕЛЬ, бортика (двома строчками, друга 13мм)  на велкро тканину",
+    img: [
+      "../img/tablet/img7a.jpg",
+      "../img/tablet/img7b.jpg",
+      "../img/tablet/img7c.jpg",
+    ],
+    description: "img7",
+  },
+  {
+    text: "8. Зшивання передньої стінки з велкро тканиною із вставкою єви(3мм)",
+    img: ["../img/tablet/img8.jpg"],
+    description: "img8",
+  },
+  {
+    text: "9. Зшивання деталей клапана та кишені (вставка єви (3мм)) пришивання двох липучок",
+    img: [
+      "../img/tablet/img9a.jpg",
+      "../img/tablet/img9b.jpg",
+      "../img/tablet/img9c.jpg",
+    ],
+    description: "img9",
+  },
+  {
+    text: "10. Вставка люверса",
+    img: ["../img/tablet/img10.jpg"],
+    description: "img10",
+  },
+  {
+    text: "11. Нашивання петель, клапана та кишені на підкладку передньої внутрішньої перегородки",
+    img: ["../img/tablet/img11a.jpg", "../img/tablet/img11b.jpg"],
+    description: "img11",
+  },
+  {
+    text: "12. Розмітка та нашивання резинки задньої перегородки",
+    img: ["../img/tablet/img12.jpg"],
+    description: "img12",
+  },
+  {
+    text: "13.Зшивання передньої перегородки з задньою перегородкою з вставкою єви(3мм)",
+    img: ["../img/tablet/img13a.jpg", "../img/tablet/img13b.jpg"],
+    description: "img13",
+  },
+  {
+    text: "14. Зшивання задньої стінки з велкро + вставка єви (5мм)",
+    img: ["../img/tablet/img14.jpg"],
+    description: "img14",
+  },
+  {
+    text: "15. Нашивання верхніх та бокових петель на задню стінку",
+    img: ["../img/tablet/img15.jpg"],
+    description: "img15",
+  },
+  {
+    text: "16.Зшивання вузла переднього бортика (блискавка,фальди, ботан, підклад), єва в ботан",
+    img: [
+      "../img/tablet/img16a.jpg",
+      "../img/tablet/img16b.jpg",
+      "../img/tablet/img16c.jpg",
+      "../img/tablet/img16d.jpg",
+      "../img/tablet/img16e.jpg",
+    ],
+    description: "img16",
+  },
+  {
+    text: "17. Зшивання вузла заднього бортика  (блискавка,фальди, ботан, підклад)",
+    img: ["../img/tablet/img17.jpg"],
+    description: "img17",
+  },
+  {
+    text: "18. Зшивання ручки, з вставкою єви",
+    img: [
+      "../img/tablet/img18a.jpg",
+      "../img/tablet/img18b.jpg",
+      "../img/tablet/img18c.jpg",
+    ],
+    description: "img18",
+  },
+  {
+    text: "19. Нашивання вузла бортика переднього відділу на вузол передньої стінки",
+    img: ["../img/tablet/img19.jpg", "../img/tablet/img19b.jpg"],
+    description: "img19",
+  },
+  {
+    text: "20. Нашивання вузла бортика заднього відділу на вузол задньої стінки",
+    img: ["../img/tablet/img20.jpg"],
+    description: "img20",
+  },
+  {
+    text: "21. Окантовка переднього відділу",
+    img: ["../img/tablet/img21.jpg"],
+    description: "img21",
+  },
+  {
+    text: "22. Окантовка заднього відділу",
+    img: ["../img/tablet/img22.jpg"],
+    description: "img22",
+  },
+  {
+    text: "23. Нашивання переднього відділу на вузол перегородки (підкладкою бортика до кишені з клапаном)",
+    img: ["../img/tablet/img23.jpg"],
+    description: "img23",
+  },
+  {
+    text: "24. Пришивання ручки на вузол переднього відділу (на зовнішню сторону бортика)",
+    img: ["../img/tablet/img24.jpg"],
+    description: "img24",
+  },
+  {
+    text: "25. Нашивання заднього відділу на передній",
+    img: ["../img/tablet/img25a.jpg", "../img/tablet/img25b.jpg"],
+    description: "img25",
+  },
+  {
+    text: "26. Окантовка перегородки",
+    img: [],
+    description: "img26",
+  },
+  {
+    text: "27. Вивертання сумки ",
+    img: [],
+    description: "img27",
+  },
+  {
+    text: "28. Зшивання плечового та бокових ременців",
+    img: [
+      "../img/tablet/img28a.jpg",
+      "../img/tablet/img28b.jpg",
+      "../img/tablet/img28c.jpg",
+      "../img/tablet/img28d.jpg",
+      "../img/tablet/img28e.jpg",
+    ],
+    description: "img28",
+  },
+  {
+    text: "29. Пришивання липучки (папа) на ременну стрічку(2*8см) (з одного краю ременну загинаємо на 2см і на 1 см накладаємо липучку)загинання стрічки(2*13,5) з одного краю 2 раза по 2,5см",
+    img: ["../img/tablet/img29.jpg"],
+    description: "img29",
+  },
+  {
+    text: "30. Зшивання козирка (пришивання 2 дет пластику на виворіт кордури(1см від краю кордури, строчка 1см від краю пластика, пришивання стрічок до кордури)",
+    img: [
+      "../img/tablet/img30a.jpg",
+      "../img/tablet/img30b.jpg",
+      "../img/tablet/img30c.jpg",
+      "../img/tablet/img30d.jpg",
+    ],
+    description: "img30",
+  },
+  {
+    text: "31. Вставка кнопки",
+    img: [],
+    description: "img31",
+  },
+  {
+    text: "31. Вставка резинки та шнурка. Збирання сумки",
+    img: [],
+    description: "img32",
+  },
+];
