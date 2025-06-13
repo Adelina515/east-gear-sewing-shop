42e8ebcefe2c1d8784f8f63bab83532be9735c51
Adelina515 <adelgodlevska@gmail.com>
2025-06-13 15:26:14 +0300

    add shreder

---
+++ shreder/technologyList.js
@@ -0,0 +1,93 @@
+export const technologyList = [
+  {
+    text: "1. Розбір крою та розмітка центру основної деталі, зміна лапкина тефлонову",
+    img: ["../img/shreder/1.jpg"],
+    description: "img1",
+  },
+  {
+    text: "2.Обробка з'ємної деталі(нашити липучку та бірку, закріпити резинку",
+    img: ["../img/shreder/2.jpg"],
+    description: "img2",
+  },
+  {
+    text: "3. Загинання 8мм строчкою кордури під турнікет та зшивання його з петлею , резинкою та липучкою (папа) 25мм. Строчка 2см від липучки",
+    img: ["../img/shreder/3.jpg"],
+    description: "img3",
+  },
+  {
+    text: "4. Пришивання , блискавки та підкладки до передньої стінки, вставка бігунка. Пришивання захисного клапана на блискавку",
+    img: ["../img/shreder/4.jpg"],
+    description: "img4",
+  },
+  {
+    text: "5. Пришивання деталі під турнікет, липучки під шеврон, закріплення хіполонових деталей до передньої стінки",
+    img: [
+      "../img/shreder/5a.jpg",
+      "../img/shreder/5b.jpg",
+      "../img/shreder/5c.jpg",
+      "../img/shreder/5d.jpg",
+      "../img/shreder/5e.jpg",
+      "../img/shreder/5f.jpg",
+    ],
+    description: "img5",
+  },
+  {
+    text: "6. Закріплення накладної деталі та петель на клапан. Зміна лапки",
+    img: ["../img/shreder/6a.jpg", "../img/shreder/6b.jpg"],
+    description: "img6",
+  },
+  {
+    text: "7. 10 закріпок (клапан, передня стінка,з'ємна деталь)",
+    img: [
+      "../img/shreder/7a.jpg",
+      "../img/shreder/7b.jpg",
+      "../img/shreder/7c.jpg",
+    ],
+    description: "img7",
+  },
+  {
+    text: "8. Зашивання виточок та підшивання підкладки до передньої стінки",
+    img: ["../img/shreder/8.jpg"],
+    description: "img8",
+  },
+  {
+    text: "9. Обробка кишені на задній стінці",
+    img: ["../img/shreder/9a.jpg", "../img/shreder/9b.jpg"],
+    description: "img9",
+  },
+  {
+    text: "10. Нашивання липучки 5см (папа), липучки 4см (мама), петель, бірки на задню стінку з пришиванням підкладки по периметру ",
+    img: ["../img/shreder/10.jpg"],
+    description: "img10",
+  },
+  {
+    text: "11. Нашивання липучки 5см (мама) на деталь пояса",
+    img: ["../img/shreder/11.jpg"],
+    description: "img11",
+  },
+  {
+    text: "12. Зшивання Зшивання клапана, задньої стінки та велкро з вставкою піни",
+    img: [
+      "../img/shreder/12a.jpg",
+      "../img/shreder/12b.jpg",
+      "../img/shreder/12c.jpg",
+      "../img/shreder/12d.jpg",
+    ],
+    description: "img12",
+  },
+  {
+    text: "13. Зшивання вузла задньої стінки з вузлом передньої стінки",
+    img: ["../img/shreder/13.jpg"],
+    description: "img13",
+  },
+  {
+    text: "14. Окантовка. Вивертання",
+    img: ["../img/shreder/14.jpg"],
+    description: "img14",
+  },
+  {
+    text: "15. Зшивання з'ємних ременних стрічок з липучкою",
+    img: ["../img/shreder/15.jpg"],
+    description: "img15",
+  },
+];
