a145208988444186c0cd57fd6e521899d1a8e621
Adelina515 <adelgodlevska@gmail.com>
2025-08-04 15:37:20 +0300

    add file rps-storm

---
+++ rps-storm/technologyList.js
@@ -0,0 +1,122 @@
+export const technologyList = [
+  {
+    text: "1. Розмітка молей",
+    img: ["../img/rps-storm/1.jpg"],
+    description: "img1",
+  },
+  {
+    text: "2. Зшивання верхніх деталей (під молі) з підкладкою.",
+    img: [
+      "../img/rps-storm/2a.jpg",
+      "../img/rps-storm/2b.jpg",
+      "../img/rps-storm/2c.jpg",
+    ],
+    description: "img2",
+  },
+  {
+    text: "3. Нашивання липучок (папа) на бокові під молі",
+    img: ["../img/rps-storm/3.jpg"],
+    description: "img3",
+  },
+  {
+    text: "4. Нашивання молей на центральну та бокові деталі",
+    img: [
+      "../img/rps-storm/4a.jpg",
+      "../img/rps-storm/4b.jpg",
+      "../img/rps-storm/4c.jpg",
+      "../img/rps-storm/4d.jpg",
+      "../img/rps-storm/4e.jpg",
+      "../img/rps-storm/4f.jpg",
+    ],
+    description: "img4",
+  },
+  {
+    text: "5. Закріпки",
+    img: [],
+    description: "img5",
+  },
+  {
+    text: "6. Нашивання липучок (мама) на центр нижньої деталі",
+    img: ["../img/rps-storm/6a.jpg", "../img/rps-storm/6b.jpg"],
+    description: "img6",
+  },
+  {
+    text: "7. Нашивання липучок (мама) на бокові нижні деталі",
+    img: ["../img/rps-storm/7.jpg"],
+    description: "img7",
+  },
+  {
+    text: "8. Зшивання нижніх бокових деталей (з липучками) до маленьких бокових деталей. Нашивання шльовок.Зшивання з верхньою та нижньою деталями.",
+    img: [
+      "../img/rps-storm/8a.jpg",
+      "../img/rps-storm/8b.jpg",
+      "../img/rps-storm/8c.jpg",
+      "../img/rps-storm/8d.jpg",
+      "../img/rps-storm/8e.jpg",
+    ],
+    description: "img8",
+  },
+  {
+    text: "9. 4 закріпки на шльовках",
+    img: ["../img/rps-storm/9.jpg"],
+    description: "img9",
+  },
+  {
+    text: "10. Зшивання вузла кордури, пришивання петель з рамками. Закріпки",
+    img: [
+      "../img/rps-storm/10a.jpg",
+      "../img/rps-storm/10b.jpg",
+      "../img/rps-storm/10c.jpg",
+      "../img/rps-storm/10d.jpg",
+      "../img/rps-storm/10e.jpg",
+      "../img/rps-storm/10f.jpg",
+    ],
+    description: "img10",
+  },
+  {
+    text: "11. Пришивання липучок до підкладки, та пришивання бірки L/M під липучку папа на лицеву сторону підкладки ",
+    img: ["../img/rps-storm/11a.jpg", "../img/rps-storm/11b.jpg"],
+    description: "img11",
+  },
+  {
+    text: "12. Пришивання липучок(мама) до сітки",
+    img: ["../img/rps-storm/12.jpg"],
+    description: "img12",
+  },
+  {
+    text: "13. Зшивання підкладки з сіткою",
+    img: ["../img/rps-storm/13.jpg"],
+    description: "img13",
+  },
+  {
+    text: "14. Вставка бігунка в блискавку,пришивання круглої резинки на початок бігунка . Пришивання деталі(трикутники) кордури та підкладки до блискавки",
+    img: ["../img/rps-storm/14.jpg"],
+    description: "img14",
+  },
+  {
+    text: "15. Пришивання блискавки до вузла кордури. Окантовка",
+    img: [
+      "../img/rps-storm/15a.jpg",
+      "../img/rps-storm/15b.jpg",
+      "../img/rps-storm/15c.jpg",
+      "../img/rps-storm/15d.jpg",
+      "../img/rps-storm/15e.jpg",
+    ],
+    description: "img15",
+  },
+  {
+    text: "16. Зшивання вузла сітки та вузла кордури по периметру лицевими сторонами",
+    img: ["../img/rps-storm/16a.jpg", "../img/rps-storm/16b.jpg"],
+    description: "img16",
+  },
+  {
+    text: "17.  Окантовка, вивертання",
+    img: ["../img/rps-storm/17.jpg"],
+    description: "img17",
+  },
+  {
+    text: "18. Зшивання ременя з липучками. Зшивання резинок. Віддати на вставку ременя в РПС, підшити краї ременя(скласти двічі по 15мм на лицеву сторону, до фастекса, та прошити)",
+    img: ["../img/rps-storm/18.jpg"],
+    description: "img18",
+  },
+];
