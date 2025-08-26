d226b256638c5c3999aeef2223337dbb996cc4ea
Adelina515 <adelgodlevska@gmail.com>
2025-08-26 13:23:41 +0300

    add evelope gena

---
+++ envelope-gena/technologyList.js
@@ -0,0 +1,122 @@
+export const technologyList = [
+  {
+    text: "1. Нашивання липучки та двох тканинних бірок на нижню деталь кордури",
+    img: ["../img/envelope-gena/1.jpg"],
+    description: "img1",
+  },
+  {
+    text: "2. Підгинання кишень строчкою по верхньому канту підкладки,  нашивання та строчка по розмітці на великій деталі підкладці",
+    img: [
+      "../img/envelope-gena/2a.jpg",
+      "../img/envelope-gena/2b.jpg",
+      "../img/envelope-gena/2c.jpg",
+    ],
+    description: "img2",
+  },
+  {
+    text: "3. Нашивання липучок 25 мама на деталь з кишенею. Нашивання липучок 25 (папа) на прямокутну підкладку. ",
+    img: [
+      "../img/envelope-gena/3a.jpg",
+      "../img/envelope-gena/3b.jpg",
+      "../img/envelope-gena/3c.jpg",
+    ],
+    description: "img3",
+  },
+  {
+    text: "4. Вшивання карабінів 10 мм у ремінну стрічку 10мм.",
+    img: ["../img/envelope-gena/4a.jpg", "../img/envelope-gena/5b.jpg"],
+    description: "img4",
+  },
+  {
+    text: "5. Розмітка строчок на кордурі. Пришивання блискавок та підкладки (перед відстрочуванням кордури на блискавці з підкладкою - вшити ремінні стрічки з карабінами.).",
+    img: [
+      "../img/envelope-gena/5a.jpg",
+      "../img/envelope-gena/5b.jpg",
+      "../img/envelope-gena/5c.jpg",
+      "../img/envelope-gena/5d.jpg",
+      "../img/envelope-gena/5e.jpg",
+      "../img/envelope-gena/5f.jpg",
+      "../img/envelope-gena/5g.jpg",
+      "../img/envelope-gena/5h.jpg",
+      "../img/envelope-gena/5i.jpg",
+      "../img/envelope-gena/5j.jpg",
+    ],
+    description: "img5",
+  },
+  {
+    text: "6. Вставка бігунків 2 шт на 1 блискавку, підшивання підкладки по периметру, пришивання петель 40мм",
+    img: ["../img/envelope-gena/6.jpg"],
+    description: "img6",
+  },
+  {
+    text: "7. Зшивання підйомника для телефону",
+    img: ["../img/envelope-gena/7.jpg"],
+    description: "img7",
+  },
+  {
+    text: "8. Обробка кишені на сітці (сітка 2дет підкладки, хіполон, петля ) ",
+    img: [
+      "../img/envelope-gena/8a.jpg",
+      "../img/envelope-gena/8b.jpg",
+      "../img/envelope-gena/8c.jpg",
+      "../img/envelope-gena/8d.jpg",
+      "../img/envelope-gena/8e.jpg",
+      "../img/envelope-gena/8f.jpg",
+      "../img/envelope-gena/8g.jpg",
+      "../img/envelope-gena/8h.jpg",
+      "../img/envelope-gena/8i.jpg",
+      "../img/envelope-gena/8j.jpg",
+      "../img/envelope-gena/8k.jpg",
+    ],
+    description: "img8",
+  },
+  {
+    text: "9. Нашивання хіполонових (менших деталей) молей на бокові деталі",
+    img: ["../img/envelope-gena/9.jpg"],
+    description: "img9",
+  },
+  {
+    text: "10. Нашивання бокових деталей на сітку, вистрочка по сітці",
+    img: [
+      "../img/envelope-gena/10a.jpg",
+      "../img/envelope-gena/10b.jpg",
+      "../img/envelope-gena/10c.jpg",
+    ],
+    description: "img10",
+  },
+  {
+    text: "11. Розмітка строчок на сітці. Пришивання по периметру підкладки до задньої стінки виворітними сторонами з вставкою піни 5 мм. Пришивання підйомника. Наскрізні строчки по розмітці. Пришивання резинки 40мм. ",
+    img: [
+      "../img/envelope-gena/11a.jpg",
+      "../img/envelope-gena/11b.jpg",
+      "../img/envelope-gena/11c.jpg",
+      "../img/envelope-gena/11d.jpg",
+      "../img/envelope-gena/11e.jpg",
+    ],
+    description: "img11",
+  },
+  {
+    text: "12. Зшивання стінок лицевими сторонами",
+    img: [
+      "../img/envelope-gena/12a.jpg",
+      "../img/envelope-gena/12b.jpg",
+      "../img/envelope-gena/12c.jpg",
+    ],
+    description: "img12",
+  },
+  {
+    text: "13. Окантовка",
+    img: ["../img/envelope-gena/13a.jpg", "../img/envelope-gena/13b.jpg"],
+    description: "img13",
+  },
+  {
+    text: "14. Вивертання",
+    img: ["../img/envelope-gena/14a.jpg", "../img/envelope-gena/14b.jpg"],
+    description: "img14",
+  },
+  {
+    text: "15. Зшивання плечового ременя",
+    img: ["../img/envelope-gena/15.jpg"],
+    description: "img15",
+  },
+];
