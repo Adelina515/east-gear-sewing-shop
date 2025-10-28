1b3ecd8a6fe6f5871defa6da4a3114d5239cee11
Adelina515 <adelgodlevska@gmail.com>
2025-10-28 15:54:01 +0200

    add backpack

---
+++ backpackAdapter18/technologyList.js
@@ -0,0 +1,267 @@
+export const technologyList = [
+  {
+    text: " ЛЯМКИ. 1. Зшивання вузла плечевих лямок(кордура, сітка, піна). Нашивання більшої резинку. Окантовка.",
+    img: [
+      "../img/backpackAdapter18/1a.jpg",
+      "../img/backpackAdapter18/1b.jpg",
+      "../img/backpackAdapter18/1c.jpg",
+    ],
+    description: "img1",
+  },
+  {
+    text: "2. Зшивання грудного ременя (рамки-Т подібні,фастекс). Зшивання меншої резинки",
+    img: ["../img/backpackAdapter18/2a.jpg", "../img/backpackAdapter18/2b.jpg"],
+    description: "img2",
+  },
+  {
+    text: "3. Зшивання ременя під пряжку",
+    img: ["../img/backpackAdapter18/3.jpg"],
+    description: "img3",
+  },
+  {
+    text: "4. Нашивання  молей, 7,5см між молями, стопори 2,5",
+    img: ["../img/backpackAdapter18/4a.jpg", "../img/backpackAdapter18/4b.jpg"],
+    description: "img4",
+  },
+  {
+    text: "5. Закріпка",
+    img: ["../img/backpackAdapter18/5.jpg"],
+    description: "img5",
+  },
+  {
+    text: "6. Зшивання накладної деталі налипучці",
+    img: ["../img/backpackAdapter18/6a.jpg", "../img/backpackAdapter18/6b.jpg"],
+    description: "img6",
+  },
+  {
+    text: "7. Нашити липучки на кордуру",
+    img: ["../img/backpackAdapter18/7.jpg"],
+    description: "img7",
+  },
+  {
+    text: "8. Зшити внутрішній ремінь з липучками. 2 папи, 1 мама",
+    img: ["../img/backpackAdapter18/8.jpg"],
+    description: "img8",
+  },
+  {
+    text: "9. Нашити на кордуру ременя",
+    img: ["../img/backpackAdapter18/9.jpg"],
+    description: "img9",
+  },
+  {
+    text: "10. Зшити кордуру з сіткою виворітними сторонами. Вставка піни. Пришивання липучки.",
+    img: [
+      "../img/backpackAdapter18/10a.jpg",
+      "../img/backpackAdapter18/10b.jpg",
+      "../img/backpackAdapter18/10c.jpg",
+      "../img/backpackAdapter18/10d.jpg",
+      "../img/backpackAdapter18/10e.jpg",
+    ],
+    description: "img10",
+  },
+  {
+    text: "11. Зшивання внутрішньої кишені",
+    img: [
+      "../img/backpackAdapter18/11a.jpg",
+      "../img/backpackAdapter18/11b.jpg",
+      "../img/backpackAdapter18/11c.jpg",
+      "../img/backpackAdapter18/11d.jpg",
+      "../img/backpackAdapter18/11e.jpg",
+      "../img/backpackAdapter18/11f.jpg",
+      "../img/backpackAdapter18/11g.jpg",
+      "../img/backpackAdapter18/11h.jpg",
+      "../img/backpackAdapter18/11i.jpg",
+    ],
+    description: "img11",
+  },
+  {
+    text: "12. Нашивання внутрішньої кишені на задню стінку",
+    img: [
+      "../img/backpackAdapter18/12a.jpg",
+      "../img/backpackAdapter18/12b.jpg",
+    ],
+    description: "img12",
+  },
+  {
+    text: "13. ПЕРЕДНЯ СТІНКА. КИШЕНЯ ДЛЯ ШОЛОМА. Окантовка сітки резинкою. Виточка 1см.",
+    img: ["../img/backpackAdapter18/13.jpg"],
+    description: "img13",
+  },
+  {
+    text: "14. Зшивання дна з сіткою. Вивертання.",
+    img: [
+      "../img/backpackAdapter18/14a.jpg",
+      "../img/backpackAdapter18/14b.jpg",
+      "../img/backpackAdapter18/14c.jpg",
+    ],
+    description: "img14",
+  },
+  {
+    text: "15. Зшивання ременів. Нашивання на кордуру.",
+    img: ["../img/backpackAdapter18/15.jpg"],
+    description: "img15",
+  },
+  {
+    text: "16. Нашивання дна на деталь з ременями.",
+    img: ["../img/backpackAdapter18/16.jpg"],
+    description: "img16",
+  },
+  {
+    text: "17. Нашивання другої деталі кишені. Вивертання.",
+    img: ["../img/backpackAdapter18/17.jpg"],
+    description: "img17",
+  },
+  {
+    text: "18. Відстрочка. Нашивання липучки. ",
+    img: [
+      "../img/backpackAdapter18/18a.jpg",
+      "../img/backpackAdapter18/18b.jpg",
+      "../img/backpackAdapter18/18c.jpg",
+    ],
+    description: "img18",
+  },
+  {
+    text: "19.ВНУТРІШНЯ ПЕРЕДНЯ СТІНКА. Зшивання кордури з липучкою.",
+    img: ["../img/backpackAdapter18/19.jpg"],
+    description: "img19",
+  },
+  {
+    text: "20. ЗОВНІШНЯ ПЕРЕДНЯ СТІНКА. Нашивання стрічок на лазеркат",
+    img: [
+      "../img/backpackAdapter18/20a.jpg",
+      "../img/backpackAdapter18/20b.jpg",
+      "../img/backpackAdapter18/20c.jpg",
+    ],
+    description: "img20",
+  },
+  {
+    text: "21. Розмітка кордуринання. Нашивання лазеркату, бірки, липучки.",
+    img: [
+      "../img/backpackAdapter18/21a.jpg",
+      "../img/backpackAdapter18/21b.jpg",
+    ],
+    description: "img21",
+  },
+  {
+    text: "22. Закріпки",
+    img: ["../img/backpackAdapter18/22.jpg"],
+    description: "img22",
+  },
+  {
+    text: "23. Вставка бігунка. Зшивання стопорів(4шт) з блискавкою",
+    img: ["../img/backpackAdapter18/23.jpg"],
+    description: "img23",
+  },
+  {
+    text: "24. Обробка кишені на передній стінці",
+    img: [
+      "../img/backpackAdapter18/24a.jpg",
+      "../img/backpackAdapter18/24b.jpg",
+      "../img/backpackAdapter18/24c.jpg",
+      "../img/backpackAdapter18/24d.jpg",
+      "../img/backpackAdapter18/24e.jpg",
+    ],
+    description: "img24",
+  },
+  {
+    text: "26. Нашивання кишені під шолом",
+    img: ["../img/backpackAdapter18/26.jpg"],
+    description: "img26",
+  },
+  {
+    text: "27. КІЛЬЦЕ. Зшивання ручки",
+    img: ["../img/backpackAdapter18/27.jpg"],
+    description: "img27",
+  },
+  {
+    text: "28. Зшивання клапана для отвору під гідратор",
+    img: ["../img/backpackAdapter18/28.jpg"],
+    description: "img28",
+  },
+  {
+    text: "29. Обробка отвору для гідратора. Пвдшивання підкладки.",
+    img: [
+      "../img/backpackAdapter18/29a.jpg",
+      "../img/backpackAdapter18/29b.jpg",
+      "../img/backpackAdapter18/29c.jpg",
+      "../img/backpackAdapter18/29d.jpg",
+    ],
+    description: "img29",
+  },
+  {
+    text: "30. Підвертання кишені. Підшивання кишені. Розмітка молей. Нашивання молей. Нашивання резинки на кишеню.",
+    img: [
+      "../img/backpackAdapter18/30a.jpg",
+      "../img/backpackAdapter18/30b.jpg",
+      "../img/backpackAdapter18/30c.jpg",
+      "../img/backpackAdapter18/30d.jpg",
+    ],
+    description: "img30",
+  },
+  {
+    text: "31. Пришивання блискавки. Окантовки.",
+    img: ["../img/backpackAdapter18/31.jpg"],
+    description: "img31",
+  },
+  {
+    text: "32. Закріпки",
+    img: ["../img/backpackAdapter18/32.jpg"],
+    description: "img32",
+  },
+  {
+    text: "33. Нашивання клапана для отвору",
+    img: [
+      "../img/backpackAdapter18/33a.jpg",
+      "../img/backpackAdapter18/33b.jpg",
+    ],
+    description: "img33",
+  },
+  {
+    text: "34. Нашивання ручки.",
+    img: ["../img/backpackAdapter18/34.jpg"],
+    description: "img34",
+  },
+  {
+    text: "35. Нашивання петель",
+    img: [
+      "../img/backpackAdapter18/35a.jpg",
+      "../img/backpackAdapter18/35b.jpg",
+    ],
+    description: "img35",
+  },
+  {
+    text: "36. Вставка бігунків. Пришивання дна. Пришивання петель.",
+    img: [
+      "../img/backpackAdapter18/36a.jpg",
+      "../img/backpackAdapter18/36b.jpg",
+      "../img/backpackAdapter18/36c.jpg",
+      "../img/backpackAdapter18/36d.jpg",
+    ],
+    description: "img36",
+  },
+  {
+    text: "37.ЗШИВАННЯ ВУЗЛІВ. Нашивання кільця на передню стінку.",
+    img: [
+      "../img/backpackAdapter18/37a.jpg",
+      "../img/backpackAdapter18/37b.jpg",
+    ],
+    description: "img37",
+  },
+  {
+    text: "38. Нашивання кільця на задню стінку.",
+    img: [
+      "../img/backpackAdapter18/38a.jpg",
+      "../img/backpackAdapter18/38b.jpg",
+      "../img/backpackAdapter18/38c.jpg",
+    ],
+    description: "img38",
+  },
+  {
+    text: "39. Окантовка.",
+    img: [
+      "../img/backpackAdapter18/39a.jpg",
+      "../img/backpackAdapter18/39b.jpg",
+    ],
+    description: "img39",
+  },
+];
