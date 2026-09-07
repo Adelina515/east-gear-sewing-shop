3c1505e184c4930d17c3a264c6d3cc43c51f7906
Adelina515 <adelgodlevska@gmail.com>
2026-09-07 16:27:07 +0300

    add enera

---
+++ enera/technologyList.js
@@ -0,0 +1,73 @@
+export const technologyList = [
+  {
+    text: "1. Окантовка резинкою кишень, резинка 10см",
+    img: ["../img/enera/1.jpg"],
+    description: "img1",
+  },
+  {
+    text: "2. Нашивання кишень на ботан",
+    img: ["../img/enera/2a.jpg", "../img/enera/2b.jpg"],
+    description: "img2",
+  },
+  {
+    text: "3. Зшивання плечового ременя",
+    img: ["../img/enera/3.jpg"],
+    description: "img3",
+  },
+  {
+    text: "4. Зшивання (правої) кишені для вивертання:-пришивання блискавки до маленької деталі (півмісяць);-нашивання на блискавку маленької деталі з лого фігурними краєм (лице до лиця). Відстрочка.-пришивання петлі з карабіном, 1см від блискавки-пришивання деталі (півмісяць) з блискавкою до основної  деталі з лого, лице до лиця. Відстрочка.-пришивання  нижньої основної деталі до блискавки. -пришивання бокової деталі до нижньої деталі з блискавкою, лице до лиця. Відстрочка. -вставка бігунка з двома пулерами, знизу вверх.",
+    img: [
+      "../img/enera/4a.jpg",
+      "../img/enera/4b.jpg",
+      "../img/enera/4c.jpg",
+      "../img/enera/4d.jpg",
+      "../img/enera/4e.jpg",
+      "../img/enera/4f.jpg",
+      "../img/enera/4g.jpg",
+      "../img/enera/4h.jpg",
+      "../img/enera/4i.jpg",
+      "../img/enera/4j.jpg",
+      "../img/enera/4k.jpg",
+    ],
+    description: "img4",
+  },
+  {
+    text: "5. Зшивання лівої кишені",
+    img: [
+      "../img/enera/5a.jpg",
+      "../img/enera/5b.jpg",
+      "../img/enera/5c.jpg",
+      "../img/enera/5d.jpg",
+      "../img/enera/5e.jpg",
+      "../img/enera/5f.jpg",
+      "../img/enera/5g.jpg",
+      "../img/enera/5h.jpg",
+    ],
+    description: "img5",
+  },
+  {
+    text: "6. Пришивання передньої та задньої стінки до блискавки. Відстрочка. Вставка бігунка.",
+    img: ["../img/enera/6.jpg"],
+    description: "img6",
+  },
+  {
+    text: "7. Пришивання стопорів",
+    img: ["../img/enera/7.jpg"],
+    description: "img7",
+  },
+  {
+    text: "8. Зшивання сумки",
+    img: ["../img/enera/8a.jpg", "../img/enera/8b.jpg"],
+    description: "img8",
+  },
+  {
+    text: "9. Вивертання",
+    img: ["../img/enera/9.jpg"],
+    description: "img9",
+  },
+  {
+    text: "10. Розмітка строчкою закріпок",
+    img: ["../img/enera/10.jpg"],
+    description: "img10",
+  },
+];
