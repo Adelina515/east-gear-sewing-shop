715a2862d4486ce6779454a7bc364677e61896d8
Adelina515 <adelgodlevska@gmail.com>
2026-09-08 21:26:19 +0300

    add file

---
+++ rollUpFirstAidKit/technologyList.js
@@ -0,0 +1,67 @@
+export const technologyList = [
+  {
+    text: "1. ---ОСНОВА---  <Br> Зшивання підкладки під основу",
+    img: ["../img/roll-upFirstAidKit/1.jpg"],
+    description: "img1",
+  },
+  {
+    text: "2. Зшивання трьох ручок з пластиком",
+    img: ["../img/roll-upFirstAidKit/2.jpg"],
+    description: "img2",
+  },
+  {
+    text: "3. Розмітка кордури",
+    img: [
+      "../img/roll-upFirstAidKit/3a.jpg",
+      "../img/roll-upFirstAidKit/3b.jpg",
+    ],
+    description: "img3",
+  },
+  {
+    text: "4. Задня стінка. Настрочування сквадрону (попередньо зістрочити між собою) на задню стінку. Настрочування ручок",
+    img: [
+      "../img/roll-upFirstAidKit/4a.jpg",
+      "../img/roll-upFirstAidKit/4b.jpg",
+      "../img/roll-upFirstAidKit/4c.jpg",
+      "../img/roll-upFirstAidKit/4d.jpg",
+      "../img/roll-upFirstAidKit/4e.jpg",
+      "../img/roll-upFirstAidKit/4f.jpg",
+      "../img/roll-upFirstAidKit/4g.jpg",
+    ],
+    description: "img4",
+  },
+  {
+    text: "5. Передня стінка. Настрочування резинок, липучок, бірки на передню стінку",
+    img: [
+      "../img/roll-upFirstAidKit/5a.jpg",
+      "../img/roll-upFirstAidKit/5b.jpg",
+      "../img/roll-upFirstAidKit/5c.jpg",
+      "../img/roll-upFirstAidKit/5d.jpg",
+    ],
+    description: "img5",
+  },
+  {
+    text: "6. Зшивання ріп-стопу з резинками.",
+    img: ["../img/roll-upFirstAidKit/6.jpg"],
+    description: "img6",
+  },
+  {
+    text: "7. Зшивання передньої та задньої стінки по низу. <Br>Пришивання деталей ріп-стопу. <Br>Зшивання передньої та задньої стінки з підкладкою по бокам. <Br>Зшивання кордури і підкладки по верху, лишаючи отвір у підкладці. Вивертання. Зашивання підкладки. Вивертання",
+    img: [
+      "../img/roll-upFirstAidKit/7a.jpg",
+      "../img/roll-upFirstAidKit/7b.jpg",
+      "../img/roll-upFirstAidKit/7c.jpg",
+      "../img/roll-upFirstAidKit/7d.jpg",
+      "../img/roll-upFirstAidKit/7e.jpg",
+      "../img/roll-upFirstAidKit/7f.jpg",
+      "../img/roll-upFirstAidKit/7g.jpg",
+      "../img/roll-upFirstAidKit/7h.jpg",
+    ],
+    description: "img7",
+  },
+  {
+    text: "8. Зшивання сквадрону з липучкою",
+    img: ["../img/roll-upFirstAidKit/8.jpg"],
+    description: "img8",
+  },
+];
---
+++ tourniquet4/technologyList.js
@@ -0,0 +1,45 @@
+export const technologyList = [
+  {
+    text: "1. Зшивання резинки зі сквадроном, липучкою та вшивання сквадрона в отвір.",
+    img: [
+      "../img/tourniquet4/1a.jpg",
+      "../img/tourniquet4/1b.jpg",
+      "../img/tourniquet4/1c.jpg",
+      "../img/tourniquet4/1d.jpg",
+    ],
+    description: "img1",
+  },
+  {
+    text: "2. Зшивання ручки з пластиком.",
+    img: ["../img/tourniquet4/2.jpg"],
+    description: "img2",
+  },
+  {
+    text: "3. Вшивання ручки на 1см між пластиком і сквадроном, вставка клапана в отвір.",
+    img: [
+      "../img/tourniquet4/3a.jpg",
+      "../img/tourniquet4/3b.jpg",
+      "../img/tourniquet4/3c.jpg",
+      "../img/tourniquet4/3d.jpg",
+    ],
+    description: "img3",
+  },
+  {
+    text: "4. Нашивання липучок на клапан.",
+    img: [
+      "../img/tourniquet4/4a.jpg",
+      "../img/tourniquet4/4b.jpg",
+      "../img/tourniquet4/4c.jpg",
+    ],
+    description: "img4",
+  },
+  {
+    text: "5. Підшивання резинки до пластику. Нашивання задньої стінки (з молями) та бірки на турнікет.",
+    img: [
+      "../img/tourniquet4/5a.jpg",
+      "../img/tourniquet4/5b.jpg",
+      "../img/tourniquet4/5c.jpg",
+    ],
+    description: "img5",
+  },
+];
