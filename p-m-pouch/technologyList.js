abb1adaa365e31b6371e9243052be7cab7de6636
Adelina515 <adelgodlevska@gmail.com>
2026-09-25 22:28:10 +0300

    add p-m-pouch

---
+++ p-m-pouch/technologyList.js
@@ -0,0 +1,31 @@
+export const technologyList = [
+  {
+    text: "1. Зшивання боковинки: <Br>-пришивання ремінної стрічки до тегріса; <Br>-пришивання хіполону до тегріса",
+    img: [
+      "../img/p-m-pouch/1a.jpg",
+      "../img/p-m-pouch/1b.jpg",
+      "../img/p-m-pouch/1c.jpg",
+    ],
+    description: "img1",
+  },
+  {
+    text: "2. Зшивання ручки з пластиком.",
+    img: ["../img/p-m-pouch/2a.jpg", "../img/p-m-pouch/2b.jpg"],
+    description: "img2",
+  },
+  {
+    text: "3. Нашивання липучки на тегріс, нашивання петлі на ручки",
+    img: ["../img/p-m-pouch/3a.jpg", "../img/p-m-pouch/3b.jpg"],
+    description: "img3",
+  },
+  {
+    text: "4. Нашивання сквадрону на тегріс, петлі 6-7мм.",
+    img: ["../img/p-m-pouch/4a.jpg", "../img/p-m-pouch/4b.jpg"],
+    description: "img4",
+  },
+  {
+    text: "5. Вставка люверсів.",
+    img: ["../img/p-m-pouch/5a.jpg", "../img/p-m-pouch/5b.jpg"],
+    description: "img5",
+  },
+];
